c11c5e8cda623fa3aff9e5e49d7d7990001828ec
J-The-Fox <116409406+J-The-Fox@users.noreply.github.com>
2023-02-03 14:17:04 -0600

    Added __init__ Example

---
+++ projects/examples/python/__init__.py
@@ -0,0 +1,2 @@
+# This is the initlization file that can be used when first creating a python file / project
+# It can also be used as a importing file, you can add one at the top of a project folder
