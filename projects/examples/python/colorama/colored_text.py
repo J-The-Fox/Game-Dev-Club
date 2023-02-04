48d52b95ce6990aee06f2aa79b6568f1092629df
J-The-Fox <116409406+J-The-Fox@users.noreply.github.com>
2023-02-04 10:28:19 -0600

    Added Colored Text Example

---
+++ projects/examples/python/colorama/colored_text.py
@@ -0,0 +1,40 @@
+"""
+Colored Text Example!
+"""
+# Built-In Modules
+import time
+# Downloaded Modules
+import colorama
+
+# The Colorama Module Colors Your Text In The Console, Here's How To Do It
+
+print("This Is Normal Text!")
+print(f"{colorama.Fore.GREEN}This Is Colored Text!")
+print(f"It Will Keep The Color Until You Use A {colorama.Fore.RESET}Reset\n")
+
+print("-----------------------------------------------")
+
+# These Are All The Colors colorama Has
+print("Here Are All The Colors!")
+print(f"{colorama.Fore.RED}Red!")
+print(f"{colorama.Fore.LIGHTRED_EX}Light Red!")
+print(f"{colorama.Fore.YELLOW}Yellow!")
+print(f"{colorama.Fore.LIGHTYELLOW_EX}Yellow!")
+print(f"{colorama.Fore.GREEN}Green!")
+print(f"{colorama.Fore.LIGHTGREEN_EX}Light Green!")
+print(f"{colorama.Fore.CYAN}Cyan!")
+print(f"{colorama.Fore.LIGHTCYAN_EX}Light Cyan!")
+print(f"{colorama.Fore.BLUE}Blue!")
+print(f"{colorama.Fore.LIGHTBLUE_EX}Light Blue!")
+print(f"{colorama.Fore.MAGENTA}Magenta!")
+print(f"{colorama.Fore.LIGHTMAGENTA_EX}Light Magenta!")
+print(f"{colorama.Fore.WHITE}White!")
+print(f"{colorama.Fore.LIGHTWHITE_EX}Light White!")
+print(f"{colorama.Fore.BLACK}Black!")
+print(f"{colorama.Fore.LIGHTBLACK_EX}Light Black!")
+
+print(f"{colorama.Fore.RESET}-----------------------------------------------")
+
+# Colors!
+print(f"{colorama.Fore.LIGHTRED_EX}C{colorama.Fore.LIGHTYELLOW_EX}O{colorama.Fore.LIGHTGREEN_EX}L{colorama.Fore.LIGHTCYAN_EX}O{colorama.Fore.LIGHTBLUE_EX}R{colorama.Fore.LIGHTMAGENTA_EX}S{colorama.Fore.RESET}")
+# Make Your Text Intersting!
